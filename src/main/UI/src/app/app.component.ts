b01fcf256633fff61c45d387c28c5875d95bb9b4
Jamie Morrissey <jmo1344@wgu.edu>
2025-04-05 01:40:57 -0700

    TASK B2

--- src/main/UI/src/app/app.component.ts
+++ src/main/UI/src/app/app.component.ts
@@ -65,9 +65,14 @@ export class AppComponent implements OnInit{
     onSubmit({value,valid}:{value:Roomsearch,valid:boolean}){
       this.getAll().subscribe(
 
-        rooms => {console.log(Object.values(rooms)[0]);this.rooms=<Room[]>Object.values(rooms)[0]; }
+        rooms => {
+          console.log(Object.values(rooms)[0]);
+          this.rooms = <Room[]>Object.values(rooms)[0];
 
-
+          this.rooms.forEach(room => {
+            room.priceEuro = room.price;
+            room.priceCanada = room.price;})
+        }
       );
     }
     reserveRoom(value:string){
@@ -120,6 +125,8 @@ export interface Room{
   roomNumber:string;
   price:string;
   links:string;
+  priceEuro: string;
+  priceCanada: string;
 
 }
 export class ReserveRoomRequest {
